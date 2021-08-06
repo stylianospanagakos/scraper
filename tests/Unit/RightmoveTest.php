6122dd7c737dfdeacb1a2a8e371540057a9659d8
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-08-06 19:37:09 +0300

    update postcode test

--- tests/Unit/RightmoveTest.php
+++ tests/Unit/RightmoveTest.php
@@ -9,24 +9,24 @@ use PHPUnit\Framework\TestCase;
 class RightmoveTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test if postcode regex is valid by providing list of all UK postcode formats.
      *
      * @return void
      */
-    public function test_postcode_entered_is_valid()
+    public function test_value_entered_is_postcode()
     {
         // list all possible UK postcode formats
         // https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
         $postcodes = ['AA9A 9AA', 'A9A 9AA', 'A9 9AA', 'A99 9AA', 'AA9 9AA', 'AA99 9AA'];
+        $invalid = [1231, 'sdkjasd'];
         $output = true;
         foreach ($postcodes as $postcode) {
             $output = $output && preg_match(FullPostcode::UK_REGEX, $postcode);
         }
-        $this->assertTrue($output);
-    }
+        foreach ($invalid as $item) {
+            $output = $output && !(bool) preg_match(FullPostcode::UK_REGEX, $item);
+        }
 
-    public function test_postcode_entered_is_invalid()
-    {
-        $this->assertFalse((bool) preg_match(FullPostcode::UK_REGEX, 'asdad'));
+        $this->assertTrue($output);
     }
 }
